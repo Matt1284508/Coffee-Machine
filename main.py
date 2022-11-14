411cf8b6eea4ce0f9de40a203123f4b31a86aa05
Matt Millsaps <mattmillsaps.dev@gmail.com>
2022-11-13 19:19:16 -0500

    import MENU & resources from data & created TODOs

---
+++ main.py
@@ -0,0 +1,19 @@
+from data import MENU, resources
+
+# TODO: Prompt user for drink selection
+
+### !!! TODO: If off is entered, program is ended
+
+### !!! TODO: If report is entered, print report
+
+# TODO: Check if resources are sufficient
+
+# TODO: Process payment
+
+# TODO: Check if transaction is successful
+
+# TODO: If resources are sufficient and trans is successful, make coffee
+
+# TODO: Print thank you/enjoy message and loop back to selection prompt
+
+# Coffee Emoji: ☕
